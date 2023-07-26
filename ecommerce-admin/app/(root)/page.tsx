497f0b24b71c3dc19a2a7a9fe5c64a0cb6420cc6
kevin <k.yaguar@wisboo.com>
2023-07-26 17:06:53 -0300

    instalations, setupPage & clerkAuth

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+const SetupPage = () => {
+    return (
+      <div>
+        <UserButton afterSignOutUrl="/"/>
+      </div>
+    )
+}
+
+export default SetupPage;
+  
